71e95f93f692aa55f1f88ec7856b25bcd5902841
JohnVanNote <johnlvannote@gmail.com>
2017-03-28 10:02:20 -0400

    Added Ale capability, and added messaging for the script

--- bin/build_vim.sh
+++ bin/build_vim.sh
@@ -15,20 +15,46 @@ VIM_HOME=$HOME/.vim
 AUTO_HOME=$VIM_HOME/autoload
 VNDL_HOME=$VIM_HOME/bundle
 DARK_HOME=$VNDL_HOME/vim-colors-solarized
+ALE_HOME=$VNDL_HOME/ale
 
 # Check for Pathogen, install if missing
-if [ ! -d $AUTO_HOME ]; then
-    mkdir -p $AUTO_HOME && \
-    curl -LSso $AUTO_HOME/pathogen.vim https://tpo.pe/pathogen.vim
+# Source: https://github.com/tpope/vim-pathogen
+if [ ! -d "$AUTO_HOME" ]; then
+    message="Installing Pathogen, see source : "
+    message+="https://github.com/tpope/vim-pathogen"
+    echo $message
+
+    mkdir -p "$AUTO_HOME" && \
+    curl -LSso "$AUTO_HOME"/pathogen.vim https://tpo.pe/pathogen.vim
 fi
 
 # Check for Vundle, install if missing
-if [ ! -d $VNDL_HOME ]; then
-    mkdir -p $VNDL_HOME && \
-    git clone https://github.com/VundleVim/Vundle.vim.git $VNDL_HOME/Vundle.vim
+# Source: https://github.com/VundleVim/Vundle.vim
+if [ ! -d "$VNDL_HOME" ]; then
+    message="Installing Vundle, see source: "
+    message+="https://github.com/VundleVim/Vundle.vim"
+    echo $message
+    
+    mkdir -p "$VNDL_HOME" && \
+    git clone https://github.com/VundleVim/Vundle.vim.git "$VNDL_HOME"/Vundle.vim
 fi
 
 # Check for Solarized, install if missing
-if [ ! -d $DARK_HOME ]; then 
-    git clone https://github.com/altercation/vim-colors-solarized.git $DARK_HOME
+# Source: https://github.com/altercation/vim-colors-solarized
+if [ ! -d "$DARK_HOME" ]; then 
+    message="Installing Solarized, see source: "
+    message+="https://github.com/altercation/vim-colors-solarized"
+    echo $message
+    
+    git clone https://github.com/altercation/vim-colors-solarized.git "$DARK_HOME"
+fi
+
+# Check for Ale, install if missing
+# Source: https://github.com/w0rp/ale
+if [ ! -d "$ALE_HOME" ]; then
+    message="Installing Ale, see source: "
+    message+="https://github.com/w0rp/ale"
+    echo $message
+
+    git clone https://github.com/w0rp/ale.git "$ALE_HOME"
 fi
