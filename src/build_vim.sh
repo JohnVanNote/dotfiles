726151a2d9be5e463c3e564f7cacce593b775b8b
John Van Note <johnlvannote@protonmail.com>
2020-09-09 21:48:17 -0400

    Some progress

--- src/build_vim.sh
+++ src/build_vim.sh
@@ -3,7 +3,7 @@
 # build_vim.sh
 #
 # John Van Note
-# 2017-03-21
+# 2020-09-09
 #
 # Install Plugins for Vim:
 # - Pathogen
@@ -18,17 +18,20 @@ DARK_HOME=$VNDL_HOME/vim-colors-solarized
 
 # Check for Pathogen, install if missing
 if [ ! -d $AUTO_HOME ]; then
+	echo "Installing Pathogen for vim at ${DARK_HOME}"
     mkdir -p $AUTO_HOME && \
     curl -LSso $AUTO_HOME/pathogen.vim https://tpo.pe/pathogen.vim
 fi
 
 # Check for Vundle, install if missing
 if [ ! -d $VNDL_HOME ]; then
+	echo "Installing Vundle for vim at ${VNDL_HOME}"
     mkdir -p $VNDL_HOME && \
     git clone https://github.com/VundleVim/Vundle.vim.git $VNDL_HOME/Vundle.vim
 fi
 
 # Check for Solarized, install if missing
-if [ ! -d $DARK_HOME ]; then 
+if [ ! -d $DARK_HOME ]; then
+	echo "Installing Solarized for vim at ${DARK_HOME}"
     git clone https://github.com/altercation/vim-colors-solarized.git $DARK_HOME
 fi
